e5d8b810705fb0181b3075520e471eaabaa0de94
CRISTIAN MACHA <cristianserva1999@gmail.com>
2020-02-07 18:52:33 -0500

    Desde la laptop

--- frontend/src/app/config/config.ts
+++ frontend/src/app/config/config.ts
@@ -1,5 +1,6 @@
 // Ruta de las apis
-export const URL_SERVICIOS = 'http://192.168.0.12:8080';
+export const URL_SERVICIOS = 'http://localhost:8080';
+// export const URL_SERVICIOS = 'http://192.168.0.12:8080'
 
 // Token
 export const TOKEN = localStorage.getItem('token');
