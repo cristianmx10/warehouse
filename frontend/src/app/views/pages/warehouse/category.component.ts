553f91fcf5958fc2218a8db98420cadceb36550e
CRISTIAN MACHA <cristianserva1999@gmail.com>
2020-02-10 20:53:25 -0500

    categoria validaciones de formulario

--- frontend/src/app/views/pages/warehouse/category.component.ts
+++ frontend/src/app/views/pages/warehouse/category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/models/category.model';
 import { CategoryService } from 'src/app/services/category.service';
+import { finalize } from 'rxjs/operators';
+declare const $: any;
 
 @Component({
   selector: 'app-category',
@@ -9,6 +11,7 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
   category: Category = {};
   categories: Category[] = [];
+  update: boolean;
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
@@ -17,8 +20,23 @@ export class CategoryComponent implements OnInit {
 
   createCAtegory() {
     this.categoryService.createCategory(this.category)
+      .pipe(finalize(() => {
+        this.cleanCategory();
+        this.getAllCategories();
+      }))
       .subscribe(
         (data: Category) => console.log(data),
+        (error) => console.error(error));
+  }
+
+  updateCategory() {
+    this.categoryService.updateCAtegory(this.category)
+      .pipe(finalize(() => {
+        this.update = false;
+        this.cleanCategory();
+      }))
+      .subscribe(
+        (data: Category) => console.log('update', data),
         (error) => console.error(error));
   }
 
@@ -27,6 +45,18 @@ export class CategoryComponent implements OnInit {
       .subscribe(
         (data: Category[]) => this.categories = data,
         (error) => console.error(error));
+  }
+
+  btnEditCategory(model: Category) {
+    this.update = true;
+    $('#myCard').CardWidget('expand');
+    $('#text').focus();
+    this.category = model;
+  }
+
+  cleanCategory() {
+    this.update = false;
+    this.category = new Category();
   }
 
 }
